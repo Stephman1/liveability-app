b323812082d5ed94fb3deea73c0e3c87c513be1d
SteveK <54144390+Stephman1@users.noreply.github.com>
2024-07-24 16:18:55 +0100

    Implemented search for UK zipcode query 2

--- server/routes.js
+++ server/routes.js
@@ -19,12 +19,12 @@ const search_us_zip = async function(req, res) {
 
     connection.query(`
         WITH LifeExpectancy AS (
-            SELECT Zip, AVG(LifeExpectancy) AS LifeExpectancy
+            SELECT Zip, AVG(LifeExpectancy) AS LifeExpectancy, ZipState AS State, ZipCity AS City
             FROM ZipTract z JOIN USLifeExpectancy u ON z.CensusTract = u.CensusTract
             WHERE Zip = ?
             GROUP BY Zip
         )
-        SELECT p.Zip AS Zip, AvgPrice, AvgRent, LifeExpectancy, NatWalkInd
+        SELECT p.Zip AS Zip, AvgPrice, AvgRent, LifeExpectancy, NatWalkInd, State, City
         FROM LatestPropertyPrices p LEFT OUTER JOIN LatestRentalPrices r ON p.Zip = r.Zip 
         LEFT OUTER JOIN LifeExpectancy l ON l.Zip = p.Zip
         LEFT OUTER JOIN USZipWalkability w ON w.Zip = p.Zip
@@ -43,6 +43,26 @@ const search_us_zip = async function(req, res) {
 // Route 2: GET /search_uk_zip/:zip
 const search_uk_zip = async function(req, res) {
     // Return all information on a given UK post code sector (zip code equivalent)
+    const zipcode = req.params.zip;
+
+    connection.query(`
+        WITH LifeExpectancy AS (
+            SELECT Sector, Combined AS LifeExpectancy, l.LocalArea AS LocalArea
+            FROM UKLifeExpectancy l JOIN UKAreasLookUp a ON l.LocalArea = a.LocalArea
+            WHERE Sector = ?
+        )
+        SELECT p.Sector AS Zip, LocalArea AS State, AvgAskingPrice AS AvgPrice, AvgAskingRent AS AvgRent, LifeExpectancy, AvgHouseholdIncome, SocialRent
+        FROM UKProperties p LEFT OUTER JOIN LifeExpectancy e ON p.Sector = e.Sector
+        WHERE p.Sector = ?
+        `, [zipcode], 
+        (err, data) => {
+        if (err || data.length === 0) {
+        console.log(err);
+        res.json({});
+        } else {
+        res.json(data[0]);
+        }
+    });
 }
 
 // Route 3: GET /search_us_zips
