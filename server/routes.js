eea6a699dea49ceabd89b381004a19d74a25245f
SteveK <54144390+Stephman1@users.noreply.github.com>
2024-07-24 14:35:35 +0100

    US and UK zip code search API calls
    * There will be a GET request for US zip codes
    * There will be a GET request for UK zip codes
    * There will be a GET request for US and UK zip codes combined

--- server/routes.js
+++ server/routes.js
@@ -12,3 +12,26 @@ const connection = mysql.createConnection({
 });
 connection.connect((err) => err && console.log(err));
 
+// Route 1: GET /search_us_zips
+const search_us_zips = async function(req, res) {
+    // Return all US zip codes that match the given search query with parameters defaulted to those specified in API spec ordered by state and zip code (ascending)
+
+}
+
+// Route 2: GET /search_uk_zips
+const search_uk_zips = async function(req, res) {
+    // Return all UK postcode sectors (zip code equivalent) that match the given search query with parameters defaulted to those specified in API spec ordered by 
+    // local area (state equivalent) and postcode sector (ascending)
+}
+
+// Route 3: GET /search_all_zips
+const search_all_zips = async function(req, res) {
+    // Return all US and UK zip codes that match the given search query with parameters defaulted to those specified in API spec ordered by state and zip code 
+    // (ascending). A UK local area is considered analagous to a US state.
+}
+
+module.exports = {
+    search_us_zips,
+    search_uk_zips,
+    search_all_zips,
+}
