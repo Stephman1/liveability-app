db1034efb6dca28d1eb24fe1c66455f78bce9f12
SteveK <54144390+Stephman1@users.noreply.github.com>
2024-08-04 16:14:46 +0100

    Starting on zip code view page

---
+++ client/src/pages/ZipCodePage.js
@@ -0,0 +1,8 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const config = require('../config.json');
+
+export default function ZipCodePage() {
+    
+}
